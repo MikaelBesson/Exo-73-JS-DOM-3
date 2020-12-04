a1078b63921750e260d9f00f54e544dab0e56e0f
BessonMikael <besson.mikael04@gmail.com>
2020-12-04 11:46:15 +0100

    exo 73

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,6 +1,24 @@
 let avatarEls = document.getElementsByTagName("img");
 
 
+for(let i = 0; i < avatarEls.length; i++){
+    avatarEls[i].style.height = "50px";
+}
+
+let bul = document.getElementById("avatar-1");
+bul.addEventListener("mouseenter", function (){
+    this.title = "Marcimus, A pink, innocent avatar";
+})
+
+let bul2 = document.getElementById("avatar-2");
+bul2.addEventListener("mouseenter", function (){
+    this.title = "Old Spice Mann, A green, angry avatar";
+})
+
+let bul3 = document.getElementById("avatar-3");
+bul3.addEventListener("mouseenter", function (){
+    this.title = "Orange Juice Squid, An orange, multi-legged avatar";
+})
 
 
 
--- exo 4/exercice.js
+++ exo 4/exercice.js
@@ -1,6 +1,23 @@
 
 
+let monDiv =document.createElement("monDiv");
+document.body.appendChild(monDiv);
+monDiv.className = "planet";
+monDiv.style.backgroundColor = "coral";
 
+let monDiv2 = document.createElement("monDiv2");
+document.body.appendChild(monDiv2);
+monDiv2.className = "moon";
+monDiv2.style.backgroundColor = "darkgrey";
+monDiv2.style.marginTop = "150px";
+
+let monDiv3 =document.createElement("monDiv3");
+document.body.appendChild(monDiv3);
+monDiv3.className = "planet";
+monDiv3.style.backgroundColor = "blue";
+monDiv3.style.width = "300px";
+monDiv3.style.height = "300px";
+monDiv3.style.margin = "auto";
 
 
 
